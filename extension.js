6a115e83c5db0673fe56f99d5c995b386f693c4b
guodfnet <guo_net_win@sina.com>
2016-03-14 16:32:26 +0800

    添加：格式化后自动保存，保存后自动格式化功能

--- extension.js
+++ extension.js
@@ -2,13 +2,14 @@
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
 var cp = require('child_process');
-var StringDecoder = require('string_decoder').StringDecoder;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
     var format = vscode.languages.registerDocumentFormattingEditProvider('php', {
         provideDocumentFormattingEdits: function(document, fmtOption, token) {
+            //console.log([1,document]);
+            document.save();          
             var configArr=["--psr2"];
             var cmd =`php ${context.extensionPath}/fmt.8.9.0.phar ${configArr.join(" ")} ${document.fileName}`;
             console.log(`cmd line:${cmd}`);          
@@ -21,6 +22,14 @@ function activate(context) {
             });
         }
     });
+
+    var saveFile=vscode.commands.registerCommand('workbench.action.files.save',function (args) {
+        var editor=vscode.window.activeTextEditor
+        if(editor){
+            vscode.commands.executeCommand('editor.action.format',[editor.document]);
+        }
+    });
+    context.subscriptions.push(saveFile);
     context.subscriptions.push(format);
 }
 exports.activate = activate;
