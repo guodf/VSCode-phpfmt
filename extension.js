bd3a6f02462e38b76dbebcc7a7947cb1c7122454
guodfnet <guo_net_win@sina.com>
2016-03-24 17:26:15 +0800

    添加格式化参数：代码风格设置，缩进设置

--- extension.js
+++ extension.js
@@ -5,32 +5,48 @@ var cp = require('child_process');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-function activate(context) {
-    var format = vscode.languages.registerDocumentFormattingEditProvider('php', {
-        provideDocumentFormattingEdits: function(document, fmtOption, token) {
-            //console.log([1,document]);
-            document.save();          
-            var configArr=["--psr2"];
-            var cmd =`php ${context.extensionPath}/fmt.8.9.0.phar ${configArr.join(" ")} ${document.fileName}`;
-            console.log(`cmd line:${cmd}`);          
-            cp.exec(cmd, function(error, stdout, stderr) {
-                console.log(`stdout: ${stdout}`);
-                console.log(`stderr: ${stderr}`);
-                if (error !== null) {
-                    console.log(`exec error: ${error}`);
-                }
-            });
-        }
-    });
 
-    var saveFile=vscode.commands.registerCommand('workbench.action.files.save',function (args) {
-        var editor=vscode.window.activeTextEditor
-        if(editor){
-            vscode.commands.executeCommand('editor.action.format',[editor.document]);
+function format(context, document) {
+    return document.save().then(() => {
+        var phpfmt = vscode.workspace.getConfiguration("phpfmt");
+
+        var styleArr = phpfmt.get("style", ["psr2"]);
+        var argArr = [];
+        for (var index in styleArr) {
+            argArr.push("--" + styleArr[index]);
         }
+
+        var indent_with_space = phpfmt.get("indent_with_space", 0);
+        if (indent_with_space > 0) {
+            argArr.push(`--indent_with_space=${indent_with_space}`);
+        }
+
+        var cmd = `php ${context.extensionPath}/fmt.phar ${argArr.join(" ")} ${document.fileName}`;
+        console.log(`cmd line:${cmd}`);
+        cp.exec(cmd, function(error, stdout, stderr) {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
     });
-    context.subscriptions.push(saveFile);
-    context.subscriptions.push(format);
+}
+function activate(context) {
+
+    //save file
+    context.subscriptions.push(vscode.commands.registerCommand('workbench.action.files.save', function(args) {
+        var editor = vscode.window.activeTextEditor
+        if (editor) {
+            return format(context, editor.document);
+        }
+    }));
+    //formar
+    context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('php', {
+        provideDocumentFormattingEdits: function(document, fmtOption, token) {
+            format(context, document);
+        }
+    }));
 }
 exports.activate = activate;
 
